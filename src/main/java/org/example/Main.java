9f210de66359768bc64a5814825b99dc677ddde3
Xumoyun <102345213+KhaliBaniya@users.noreply.github.com>
2023-03-07 15:44:08 +0500

    Main commited

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import org.example.controller.AuthController;
+import org.example.db.DB;
+import org.example.db.InitDB;
+
 public class Main {
     public static void main(String[] args) {
+        DB.initTable();
 
+        InitDB.adminInit();
+        InitDB.addCompanyCard();
+
+        AuthController authController = new AuthController();
+        authController.start();
 
     }
 }
