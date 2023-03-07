27b68d0d07cd8b6fe01c972ad441443138861197
Xumoyun <102345213+KhaliBaniya@users.noreply.github.com>
2023-03-07 09:56:37 +0500

    Transaction dto get set

--- src/main/java/org/example/dto/Transaction_dto.java
+++ src/main/java/org/example/dto/Transaction_dto.java
@@ -12,4 +12,63 @@ public class Transaction_dto {
     private TransactionType transactionType;
     private LocalDateTime createdDate;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(Integer cardId) {
+        this.cardId = cardId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Integer getTerminalId() {
+        return terminalId;
+    }
+
+    public void setTerminalId(Integer terminalId) {
+        this.terminalId = terminalId;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction_dto{" +
+                "id=" + id +
+                ", cardId=" + cardId +
+                ", amount=" + amount +
+                ", terminalId=" + terminalId +
+                ", transactionType=" + transactionType +
+                ", createdDate=" + createdDate +
+                '}';
+    }
 }
