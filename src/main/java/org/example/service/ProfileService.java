2b1134029d9c9581562c4610564682421446500e
Xumoyun <102345213+KhaliBaniya@users.noreply.github.com>
2023-03-09 20:27:50 +0500

    commit spring series atto

---
+++ src/main/java/org/example/service/ProfileService.java
@@ -0,0 +1,44 @@
+package org.example.service;
+
+
+import org.example.container.ComponentContainer;
+import org.example.controller.AdminController;
+import org.example.dto.Profile;
+import org.example.enums.GeneralStatus;
+import org.example.repository.ProfileRepository;
+
+import java.util.List;
+
+public class ProfileService {
+    private ProfileRepository profileRepository;
+    private AdminController adminController;
+
+    public void profileList() {
+        List<Profile> profileList = profileRepository.getProfileList();
+
+        for (Profile profile : profileList) {
+            System.out.println(profile);
+        }
+    }
+
+    public void changeProfileStatus(String phone) {
+        Profile profile = profileRepository.getProfileByPhone(phone);
+        if (profile == null) {
+            System.out.println("Profile not found");
+            return;
+        }
+        if (profile.getStatus().equals(GeneralStatus.ACTIVE)) {
+            profileRepository.changeProfileStatus(phone, GeneralStatus.BLOCK);
+        } else {
+            profileRepository.changeProfileStatus(phone, GeneralStatus.ACTIVE);
+        }
+    }
+
+    public void setProfileRepository(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    public void setAdminController(AdminController adminController) {
+        this.adminController = adminController;
+    }
+}
